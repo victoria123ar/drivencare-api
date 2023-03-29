a74cb431b79efc7f5b1b14f3524842c2eefd6fdc
Victoria Andrade <110947470+victoria123ar@users.noreply.github.com>
2023-03-29 17:22:06 -0300

    create app and db

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
